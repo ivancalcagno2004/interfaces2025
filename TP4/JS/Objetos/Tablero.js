6ccea1396a1af80f973e1c758ef679ee7a564274
Iván Calcagno <icalcagno@alumnos.exa.unicen.edu.ar>
2025-10-25 17:36:56 -0300

    Se agrega fondo del juego

--- TP4/JS/Objetos/Tablero.js
+++ TP4/JS/Objetos/Tablero.js
@@ -1,3 +1,35 @@
 export class Tablero{
+    constructor(ctx, width, height){
+        this.ctx = ctx;
+        this.width = width;
+        this.height = height;
+    }
+
+    cargarFondo(){
+        const imagenFondo = new Image(); //<img>
+        imagenFondo.src = '../images/peg/fondo-peg.jpg';
+        imagenFondo.onload = () => {
+            this.ctx.drawImage(imagenFondo, 0, 0, this.width, this.height);
+            this.dibujar(); // Dibuja el tablero después de cargar el fondo
+        }
+    }
+
+    dibujar() {
+                
+        // Calcula el tamaño y posición del cuadrado
+        const celdaSize = Math.min(this.width, this.height) / 3;
+        const centroX = (this.width - celdaSize) / 2;
+        const centroY = (this.height - celdaSize) / 2;
     
+        this.ctx.save();
+        this.ctx.strokeStyle = "#333";
+        this.ctx.lineWidth = 2;
+        this.ctx.fillStyle = "#888888FF";
+    
+        // Dibuja el cuadrado centrado
+        this.ctx.fillRect(centroX, centroY, celdaSize, celdaSize);
+        this.ctx.strokeRect(centroX, centroY, celdaSize, celdaSize);
+    
+        this.ctx.restore();
+    }
 }
--- TP4/JS/Objetos/main.js
+++ TP4/JS/Objetos/main.js
@@ -1,6 +1,8 @@
 "use strict"
 
+import { Auto } from "./Auto.js";
 import { Juego } from "./Juego.js";
+import { Tablero } from "./Tablero.js";
 
 const perfil = document.querySelector('.bi-person-circle');
 const perfil_box = document.querySelector('.perfil_usuario');
@@ -12,6 +14,7 @@ perfil.addEventListener('click', () => {
 });
 
 ///////////////////////////// CONSTANTES Y VARIABLES DEL JUEGO /////////////////////////////
+
 // Configuración del canvas
 const canvas = document.getElementById('ejecucion-juego');
 const ctx = canvas.getContext('2d');
@@ -20,3 +23,7 @@ const width = canvas.width;
 const height = canvas.height;
 
 const juego = new Juego();
+
+const tablero = new Tablero(ctx, width, height);
+tablero.cargarFondo();
+tablero.dibujar();
